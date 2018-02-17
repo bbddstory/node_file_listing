7215fcb983c87d6dbc05cce1c04bb0f2c1b94254
LAUREL\Meleon <bbddstory@gmail.com>
2018-02-17 22:00:20 +1100

    Most basic file listing function.

---
+++ file_listing.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.readdir(__dirname, (err, files) => {
+
+  for(let i in files){
+    fs.appendFile('filelist.txt', files[i] + '\n', (err) => {
+      if (err) throw err;
+    });
+  }
+
+  console.log('-- Listing completed.');
+
+});
