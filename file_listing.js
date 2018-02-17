46dcb665ed1bcd138ea136bdea82f4a23b8df708
LAUREL\Meleon <bbddstory@gmail.com>
2018-02-18 02:51:37 +1100

    Added recursive listing.

--- file_listing.js
+++ file_listing.js
@@ -1,13 +1,12 @@
+"use strict";
+
 const fs = require('fs');
 
 fs.readdir(__dirname, (err, files) => {
-
-  for(let i in files){
+  for (let i in files)
     fs.appendFile('filelist.txt', files[i] + '\n', (err) => {
       if (err) throw err;
     });
-  }
 
   console.log('-- Listing completed.');
-
 });
