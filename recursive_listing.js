b3725f5b498640c2b096fb1b99f9e2bb4e815671
LAUREL\Meleon <bbddstory@gmail.com>
2018-02-19 00:52:40 +1100

    Added recursive naming.

--- recursive_listing.js
+++ recursive_listing.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let fs = require('fs');
+// import fs from 'fs';
 
 const recursiveListing = (dir, filelist) => {
   fs = fs || require('fs');
@@ -8,22 +9,14 @@ const recursiveListing = (dir, filelist) => {
   filelist = filelist || [];
 
   files.forEach((file) => {
-    if (fs.statSync(dir + '/' + file).isDirectory()) {
-      if(file === 'BBC.Timeshift')
-        fs.rename(dir + '/' + file, dir + '/' + 'BBC - Timeshift', (err) => {
-          if (err) throw err;
-        });
+    if (fs.statSync(dir + '/' + file).isDirectory()){
       if(file !== '#recycle' && !file.startsWith('@')) { // Customisable rules
         filelist.push(file);
         filelist = recursiveListing(dir + '/' + file, filelist);
-      }
+      };
     } else {
-      if(file === 'BBC.Timeshift.2009.The.Last.Days.of.the.Liners.720p.HDTV.x264.AAC.MVGroup.org.mkv')
-        fs.rename(dir + '/' + file, dir + '/' + '2009 - BBC Timeshift The Last Days of the Liners.mkv', (err) => {
-          if (err) throw err;
-        });
       filelist.push(file);
-    }
+    };
   });
 
   return filelist;
@@ -36,19 +29,24 @@ const listFilter = (filelist) => {
       endsWith = ['.txt', '.ini', '.cfg', '.js', '.db', 'SynoEAStream'];
 
   for(let i in filelist) {
-    for(let j in startsWith)
-      if(filelist[i].startsWith(startsWith[j]))
+    for(let j in startsWith) {
+      if(filelist[i].startsWith(startsWith[j])) {
         valid = false;
+      }
+    };
 
-    for(let k in endsWith)
-      if(filelist[i].endsWith(endsWith[k]))
+    for(let k in endsWith) {
+      if(filelist[i].endsWith(endsWith[k])) {
         valid = false;
+      }
+    };
 
-    if(valid)
+    if(valid) {
       templist.push(filelist[i]);
+    };
 
     valid = true;
-  }
+  };
 
   return templist;
 };
@@ -56,10 +54,11 @@ const listFilter = (filelist) => {
 let recursivelist = recursiveListing('./');
 let finallist = listFilter(recursivelist);
 
-for(let i in finallist)
+for(let i in finallist) {
   fs.appendFile('filelist.txt', finallist[i] + '\n', (err) => {
     if (err) throw err;
   });
+};
 
 console.log('\n-- Listing completed.');
 console.log('-- ' + finallist.length + ' files in total.\n');
